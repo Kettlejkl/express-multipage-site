acec0235be3b83a6baaadcf0e8a9a09be12d0e68
Ian Estacio <mimestacio@tip.edu.ph>
2025-03-13 22:40:40 +0800

    Updated server.js to use require for JSON

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" folder
+// Load blog posts at startup
+const blogPosts = require('./data/posts.json');
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve HTML pages
@@ -14,15 +16,9 @@ app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'abou
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
 app.get('/blog', (req, res) => res.sendFile(path.join(__dirname, 'views', 'blog.html')));
 
-// API to load blog posts dynamically
+// API to serve blog data
 app.get('/api/blog', (req, res) => {
-    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).json({ error: 'Failed to load blog posts' });
-            return;
-        }
-        res.json(JSON.parse(data));
-    });
+    res.json(blogPosts);
 });
 
 // Start server
